30aeb63b230193ae6c8802ec9fe142ca071c4297
DizzyRaven <akaynt47@gmail.com>
2019-03-03 19:12:59 +0200

    Orc And Human

--- Warriors.py
+++ Warriors.py
@@ -1,13 +1,15 @@
-class WarriorBase:
+from Weapons import Weapon
 
+class WarriorBase:
     def __init__(self, weapon, name):
         self.name = name
         self.weapon = weapon
-        self.hp = 100
+        self.hp = 100 
 
     def kick(self):
         pass
-
+    def __str__(self):
+        return f"Name: {self.name}\nHP: {self.hp}\nRace: {type(self).__name__}\nWeapon: {type(self.weapon).__name__}"
 
 class Orc(WarriorBase):
 
@@ -17,5 +19,13 @@ class Orc(WarriorBase):
 
     def kick(self):
         return self.weapon.get_damage() * 0.9
+
+class Human(WarriorBase):
+    def __init__(self, weapon, name):
+        super(Human, self).__init__(weapon, name)
+
+
+    def kick(self):
+        return self.weapon.get_damage()*1.2
 
 
