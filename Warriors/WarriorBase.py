805a1bfa9814d2d94e9da7a9e9f6ae303061d4ed
MrCrossGK <mr.cross@ukr.net>
2019-03-03 18:08:51 +0200

    1

--- Warriors/WarriorBase.py
+++ Warriors/WarriorBase.py
@@ -1,8 +1,8 @@
 class WarriorBase:
-    def __init__(self, hp_plus, weapon, name):
+    def __init__(self, weapon, name):
         self.name = name
         self.weapon = weapon
-        self.hp = 100 + hp_plus
+        self.hp = 100
 
     def kick(self):
         pass
