35ba864dde2c45f75e0c5879a513259b0402ec95
stevepoker <stevepokersx@gmail.com>
2019-03-03 19:36:35 +0200

    up

--- Game.py
+++ Game.py
@@ -2,15 +2,5 @@ from Weapons import *
 from Warriors import *
 
 
-sword = Sword()
-human = Human(sword,"Ilya")
-axe = Axe()
-orc = Orc(axe, 'Stepan')
-
-print (human)
-
-print (human.kick())
-
-print (orc)
-
-print (orc.kick())
+orc1 = Orc(axe1, 'Lolik')
+axe1 = Axe()
